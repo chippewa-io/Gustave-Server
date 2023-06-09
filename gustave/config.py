d1ed554e70a052649dffa7bc5a601759654cee66
Chris <m.christopher.cohoon@me.com>
2023-06-09 15:15:51 -0500

    config updates

--- gustave/config.py
+++ gustave/config.py
@@ -4,14 +4,14 @@ class Config:
     TESTING = True
     TOKEN_EXPIRATION = 90
     MYSQL_DATABASE_HOST = '127.0.0.1'  # change this line
-    MYSQL_DATABASE_USER = 'chris'
-    MYSQL_DATABASE_PASSWORD = 'gustave2'
+    MYSQL_DATABASE_USER = 'beaver'
+    MYSQL_DATABASE_PASSWORD = 'gustave03'
     MYSQL_DATABASE_DB = 'secrets'
     MYSQL_DATABASE_PORT = 3306
     JAMF_PRO_URL = 'https://tcohoon.jamfcloud.com'
     JAMF_PRO_USERNAME = 'admin'
     JAMF_PRO_PASSWORD = 'jamf1234'
-    CATEGORY_ID = 4
+    CATEGORY_ID = 5
     CATEGORY_NAME = "Gustave Tokens"
     PROFILE_DESCRIPTION = "This profile is used on the backend of your system.  Please ignore this."
 
