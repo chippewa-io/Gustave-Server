ecb894175483aa5d8f8260ea95b6ab45699a32c7
Chris <m.christopher.cohoon@me.com>
2023-06-19 22:07:20 -0500

    Update computers.py

--- gustave/routes/computers.py
+++ gustave/routes/computers.py
@@ -127,7 +127,7 @@ def update_computer():
 
         
         # Return the response
-        return jsonify(response.json())
+        return jsonify({'message': 'Request was successful, but the response was not expected'})
 
     except Exception as e:
         # Catch any other exceptions and return a generic error message
