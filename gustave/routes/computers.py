90df25e43cce340fa39d5fc945884d7866e1735f
Chris <m.christopher.cohoon@me.com>
2023-06-19 21:28:09 -0500

    Update computers.py

--- gustave/routes/computers.py
+++ gustave/routes/computers.py
@@ -53,3 +53,64 @@ def get_computer_by_id():
     except Exception as e:
         # Catch any other exceptions and return a generic error message
         return jsonify({'error': f'An unexpected error occurred: {str(e)}'})
+
+@app.route('/computers', methods=['POST'])
+def update_computer():
+    try:
+        # get UDID, Secret, ID, and Value from client request
+        udid = request.json.get('udid')
+        secret = request.json.get('secret')
+        id = request.json.get('id')
+        value = request.json.get('value')
+
+        # create a cursor for MySQL
+        conn = mysql.connect()
+        cur = conn.cursor()
+
+        # execute query to get the computer ID by UDID and Secret
+        query = "SELECT computer_id FROM secret_table WHERE udid = %s AND secret = %s AND expiration > UNIX_TIMESTAMP()"
+        cur.execute(query, (udid, secret,))
+        result = cur.fetchone()
+
+        if result is None:
+            return jsonify({'error': 'No computer found with the provided UDID and secret'})
+
+        computer_id = result[0]
+        jamf_pro_url = current_app.config.get('JAMF_PRO_URL')
+        jamf_pro_username = current_app.config.get('JAMF_PRO_USERNAME')
+        jamf_pro_password = current_app.config.get('JAMF_PRO_PASSWORD')
+
+        # Get the authentication token
+        token = generate_jamf_pro_token()
+
+        if token is None:
+            return jsonify({'error': 'Failed to generate Jamf Pro token'})
+
+        # Construct the XML data
+        root = ET.Element("computer")
+        extension_attributes = ET.SubElement(root, "extension_attributes")
+        extension_attribute = ET.SubElement(extension_attributes, "extension_attribute")
+        ET.SubElement(extension_attribute, "id").text = id
+        ET.SubElement(extension_attribute, "value").text = value
+        xml_data = ET.tostring(root, encoding="utf-8", method="xml")
+
+        # Construct the headers
+        headers = {
+            'Accept': 'application/xml',
+            'Content-Type': 'application/xml',
+            'Authorization': f'Bearer {token}',
+        }
+
+        # Send the PUT request
+        url = f"{jamf_pro_url}/JSSResource/computers/id/{computer_id}"
+        response = requests.put(url, headers=headers, data=xml_data)
+
+        if response.status_code != 200:
+            return jsonify({'error': f'Jamf Pro API request failed with status code {response.status_code}'})
+
+        # Return the response
+        return jsonify(response.json())
+
+    except Exception as e:
+        # Catch any other exceptions and return a generic error message
+        return jsonify({'error': f'An unexpected error occurred: {str(e)}'})
