a202cd3d36a38954dbbe6bc582d16fc8080f724d
Chris <m.christopher.cohoon@me.com>
2023-06-25 17:14:39 -0500

    Update app.py

--- gustave/app.py
+++ gustave/app.py
@@ -5,6 +5,7 @@ from flask_apscheduler import APScheduler
 from waitress import serve
 import os
 import logging
+import sys
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
