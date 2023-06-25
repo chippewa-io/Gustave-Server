d3eb9af72a94717575fc562d0b2c223d21f9f859
Chris <m.christopher.cohoon@me.com>
2023-06-25 17:10:43 -0500

    Update app.py

--- gustave/app.py
+++ gustave/app.py
@@ -6,6 +6,10 @@ from waitress import serve
 import os
 import logging
 
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 # Import blueprints
 from routes.computers import computers_bp
 from routes.token_generation import token_generation_bp
