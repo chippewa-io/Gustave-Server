a8a1f0b5bc8211aca8675d1a19f432a6f898d098
Chris <m.christopher.cohoon@me.com>
2023-06-28 16:58:46 -0500

    updating for bash script

--- Install/config.py
+++ Install/config.py
@@ -13,19 +13,16 @@ class Config:
     PROFILE_DESCRIPTION = "This profile is used on the backend of your system.  Please ignore this."
 
 class DevelopmentConfig(Config):
-    CONFIG_CLASS = 'config.DevelopmentConfig'
     USE_WAITRESS = False
     DEBUG = True
     TESTING = True
     TOKEN_EXPIRATION = 5 #in seconds.  31556926=year 2629743=month 86400=day 3600=hour
 
 class TestingConfig(Config):
-    CONFIG_CLASS = 'config.TestingConfig'
     USE_WAITRESS = False
     TESTING = True
     TOKEN_EXPIRATION = 2629743 #in seconds.  31556926=year 2629743=month 86400=day 3600=hour
 
 class ProductionConfig(Config):
-    CONFIG_CLASS = 'config.ProductionConfig'
     USE_WAITRESS = True
     TOKEN_EXPIRATION = 2629743 #in seconds.  31556926=year 2629743=month 86400=day 3600=hour
