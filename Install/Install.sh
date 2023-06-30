fda75d7b5d17c65e801ea5076539d375a08934f2
Chris <m.christopher.cohoon@me.com>
2023-06-30 13:34:25 -0500

    Update Install.sh

--- Install/Install.sh
+++ Install/Install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> install.log
+}
+
 # Check if the script is running as root
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run as root."
+    log "Please run as root."
     exit 1
 fi
 
 # Check if the script is running on Ubuntu 22.04
 if ! grep -q 'Ubuntu 22.04' /etc/os-release; then
-    echo "Gustave is only supported on Ubuntu 22.04."
+    log "Gustave is only supported on Ubuntu 22.04."
     exit 1
 fi
 
 # Check if dialog is installed and install it if not
 if ! command -v dialog >/dev/null 2>&1; then
+    log "dialog not installed.  Installing dialog"
     echo -n "Installer initializing..."
     # Try to install dialog
     apt-get install -y dialog &> /dev/null &
@@ -27,15 +33,16 @@ if ! command -v dialog >/dev/null 2>&1; then
       printf "\b${spin:$i:1}"
       sleep .1
     done
+    log "dialog has been installed"
     echo -e "\b done."
 fi
 
 # Check again if dialog is installed
 if ! command -v dialog >/dev/null 2>&1; then
-    echo "Dialog is not installed. Please install it and run the script again."
+    log "Dialog is not installed. Please install it and run the script again."
     exit 1
 fi
-
+log "starting Dialog now"
 dialog --title "Welcome" --msgbox "Greetings, esteemed guest! Welcome to the illustrious Gustave installation process. Shall we begin?" 10 40
 
 # Prompt the user for the new values
@@ -81,3 +88,92 @@ class ProductionConfig(Config):
     USE_WAITRESS = True
     TOKEN_EXPIRATION = 2629743 #in seconds.  31556926=year 2629743=month 86400=day 3600=hour
 EOF
+log "config.py generated"
+# Create the gustave user
+dialog --infobox "Creating the Gustave user..." 10 40
+sudo adduser --system --group gustave
+if [ $? -eq 0 ]; then
+    log "Gustave user created successfully."
+else
+    log "Failed to create Gustave user."
+fi
+
+# Move the gustave executable to the proper location
+dialog --infobox "Moving the Gustave executable to the proper location..." 10 40
+sudo mv /path/to/gustave /usr/local/gustave
+if [ $? -eq 0 ]; then
+    log "gustave moved properly."
+else
+    log "Failed to move gustave."
+fi
+
+# Set the owner to gustave
+dialog --infobox "Setting the owner to Gustave..." 10 40
+sudo chown gustave:gustave /proper/location/gustave
+if [ $? -eq 0 ]; then
+    log "permissions configured for gustave."
+else
+    log "Failed to configure for gustave."
+fi
+
+# Set the permissions
+dialog --infobox "Setting the permissions..." 10 40
+sudo chmod 755 /proper/location/gustave
+if [ $? -eq 0 ]; then
+    log "modified gustave."
+else
+    log "Failed to modify gustave."
+fi
+
+# Move the gustave.service file to the systemd directory
+dialog --infobox "Moving the Gustave service file to the systemd directory..." 10 40
+sudo mv /path/to/gustave.service /etc/systemd/system/gustave.service
+if [ $? -eq 0 ]; then
+    log "Created gustave service."
+else
+    log "Failed to create gustave service."
+fi
+
+# Set the owner and permissions for the service file
+dialog --infobox "Setting the owner and permissions for the service file..." 10 40
+sudo chown root:root /etc/systemd/system/gustave.service
+if [ $? -eq 0 ]; then
+    log "Set owner /etc/systemd/system/gustave.service to root:root."
+else
+    log "Failed to Set owner /etc/systemd/system/gustave.service to root:root."
+fi
+sudo chmod 644 /etc/systemd/system/gustave.service
+if [ $? -eq 0 ]; then
+    log "modified /etc/systemd/system/gustave.service to 644."
+else
+    log "Failed to modify /etc/systemd/system/gustave.service to 644."
+fi
+
+# Move the config.py file to the proper location
+dialog --infobox "Moving the config.py file to the proper location..." 10 40
+sudo mv /path/to/config.py /proper/location/config.py
+if [ $? -eq 0 ]; then
+    log "Moved config.py into place."
+else
+    log "Failed to move config.py into place"
+fi
+
+# Set the owner and permissions for the config file
+dialog --infobox "Setting the owner and permissions for the config file..." 10 40
+sudo chown gustave:gustave /proper/location/config.py
+
+sudo chmod 644 /proper/location/config.py
+
+# Reload the systemd daemon to recognize the new service
+dialog --infobox "Reloading the systemd daemon to recognize the new service..." 10 40
+sudo systemctl daemon-reload
+
+# Enable the service so it starts on boot
+dialog --infobox "Enabling the service so it starts on boot..." 10 40
+sudo systemctl enable gustave
+
+# Start the service
+dialog --infobox "Starting the service..." 10 40
+sudo systemctl start gustave
+
+dialog --msgbox "Installation complete! Gustave is now up and running." 10 40
