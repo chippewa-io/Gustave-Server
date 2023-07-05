f60e38ea26bdaf993a916975c3923fd08baa957e
Chris <m.christopher.cohoon@me.com>
2023-07-05 11:22:00 -0500

    installer2

--- Install/install2.sh
+++ Install/install2.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 ###############################################################################
-#Variables
+#                                   Variables                                 #
 ###############################################################################
 progress_file="/tmp/install_progress.txt"
 ###############################################################################
 # Functions
 ###############################################################################
-
 check_mysql_installed() {
   if command -v mysql > /dev/null 2>&1; then
     return 0
@@ -18,7 +17,6 @@ check_mysql_installed() {
 log() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> install.log
 }
-
 ###############################################################################
 # Check if the script is running as root
 ###############################################################################
@@ -26,8 +24,9 @@ if [ "$(id -u)" -ne 0 ]; then
     log "Please run as root."
     exit 1
 fi
-
+###############################################################################
 # Check if the script is running on Ubuntu 22.04
+###############################################################################
 if ! grep -q 'Ubuntu 22.04' /etc/os-release; then
     log "Gustave is only supported on Ubuntu 22.04."
     exit 1
@@ -97,19 +96,10 @@ if ! command -v python3-apt >/dev/null 2>&1; then
     echo -e "\b done."
 fi
 ###############################################################################
-# Check again if dialog is installed
-###############################################################################
-if ! command -v dialog >/dev/null 2>&1; then
-    log "Dialog is not installed. Please install it and run the script again."
-    exit 1
-fi
-###############################################################################
 # Start Dialog
 ###############################################################################
 log "starting Dialog now"
 dialog --title "Welcome" --msgbox "Greetings, esteemed guest! Welcome to the illustrious Gustave installation process. Shall we begin?" 10 40
-###############################################################################
-# Check if mysql is installed
 ###############################################################################
 if check_mysql_installed; then
   log "MySQL is already installed."
@@ -166,6 +156,10 @@ else
     install_mysql=0
   fi
 fi
+
+#//////////////////////////////////////////////////////////////////////////////
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@    MySQL          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 ###############################################################################
 # Prompt the user for MySQL Values
 ###############################################################################
@@ -174,7 +168,8 @@ mysql_user=$(dialog --stdout --inputbox "MySQL User:" 0 0)
 mysql_password=$(dialog --stdout --passwordbox "MySQL Password:" 0 0)
 mysql_db=$(dialog --stdout --inputbox "MySQL Database:" 0 0)
 ###############################################################################
-# Create the database
+# Create the database                                                         #
+###############################################################################
 if [ "$install_mysql" != "0" ]; then
   log "Creating the database."
   dialog --infobox "Creating the database..." 10 40
@@ -191,10 +186,9 @@ if [ "$install_mysql" != "0" ]; then
     exit 1
   fi
 fi
-
-###############################################################################
-# Prompt the user for Jamf Pro Values
-###############################################################################
+#//////////////////////////////////////////////////////////////////////////////#
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@    Jamf Pro          @@@@@@@@@@@@@@@@@@@@@@@@@@@@#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 jamf_pro_url=""
 while [[ ! $jamf_pro_url =~ ^https:// ]]; do
   jamf_pro_url=$(dialog --stdout --inputbox "Dear esteemed guest, may we kindly request the URL of your Jamf Pro server?" 10 60)
@@ -242,178 +236,4 @@ if [[ "$category_id" == null ]]; then
   dialog --title "API Error" --msgbox "Bro, there was an error creating the category. Check your Jamf Pro URL and credentials and try again." 10 40
   exit 1
 fi
-###############################################################################
-# Output the values into config.py
-###############################################################################
-cat << EOF > config.py
-class Config:
-    """Base configuration."""
-    MYSQL_DATABASE_HOST = '$mysql_host'
-    MYSQL_DATABASE_USER = '$mysql_user'
-    MYSQL_DATABASE_PASSWORD = '$mysql_password'
-    MYSQL_DATABASE_DB = '$mysql_db'
-    MYSQL_DATABASE_PORT = 3306
-    JAMF_PRO_URL = '$jamf_pro_url'
-    JAMF_PRO_USERNAME = '$jamf_pro_username'
-    JAMF_PRO_PASSWORD = '$jamf_pro_password'
-    CATEGORY_ID = $category_id
-    CATEGORY_NAME = "$category_name"
-    PROFILE_DESCRIPTION = "This profile is used on the backend of your system.  Please ignore this."
 
-class DevelopmentConfig(Config):
-    USE_WAITRESS = False
-    DEBUG = True
-    TESTING = True
-    TOKEN_EXPIRATION = 5 #in seconds.  31556926=year 2629743=month 86400=day 3600=hour
-
-class TestingConfig(Config):
-    USE_WAITRESS = False
-    TESTING = True
-    TOKEN_EXPIRATION = 2629743 #in seconds.  31556926=year 2629743=month 86400=day 3600=hour
-
-class ProductionConfig(Config):
-    USE_WAITRESS = True
-    TOKEN_EXPIRATION = 2629743 #in seconds.  31556926=year 2629743=month 86400=day 3600=hour
-EOF
-log "config.py generated"
-###############################################################################
-# Create the gustave directory
-###############################################################################
-sudo mkdir -p /etc/gustave
-if [ $? -eq 0 ]; then
-    log "Created the gustave directory."
-else
-    log "Failed to create the gustave directory."
-fi
-###############################################################################
-# Move the gustave executable to the proper location
-###############################################################################
-dialog --infobox "Moving the Gustave executable to the proper location..." 10 40
-sleep 1
-sudo mv ./gustave /usr/local/bin/gustave
-if [ $? -eq 0 ]; then
-    log "gustave moved properly."
-else
-    log "Failed to move gustave."
-fi
-###############################################################################
-# Set the owner to gustave
-###############################################################################
-dialog --infobox "Setting the owner to Gustave..." 10 40
-sleep 1
-sudo chown gustave:gustave /usr/local/bin/gustave
-if [ $? -eq 0 ]; then
-    log "permissions configured for gustave."
-else
-    log "Failed to configure for gustave."
-fi
-###############################################################################
-# Set the permissions
-###############################################################################
-dialog --infobox "Setting the permissions..." 10 40
-sleep 1
-sudo chmod 755 /usr/local/bin/gustave
-if [ $? -eq 0 ]; then
-    log "modified gustave."
-else
-    log "Failed to modify gustave."
-fi
-###############################################################################
-# Move the gustave.service file to the systemd directory
-###############################################################################
-dialog --infobox "Moving the Gustave service file to the systemd directory..." 10 40
-sleep 1
-sudo mv ./gustave.service /etc/systemd/system/gustave.service
-if [ $? -eq 0 ]; then
-    log "Created gustave service."
-else
-    log "Failed to create gustave service."
-fi
-###############################################################################
-# Set the owner and permissions for the service file
-###############################################################################
-dialog --infobox "Setting the owner and permissions for the service file..." 10 40
-sleep 1
-sudo chown root:root /etc/systemd/system/gustave.service
-if [ $? -eq 0 ]; then
-    log "Set owner /etc/systemd/system/gustave.service to root:root."
-else
-    log "Failed to Set owner /etc/systemd/system/gustave.service to root:root."
-fi
-sleep 1
-sudo chmod 644 /etc/systemd/system/gustave.service
-if [ $? -eq 0 ]; then
-    log "modified /etc/systemd/system/gustave.service to 644."
-else
-    log "Failed to modify /etc/systemd/system/gustave.service to 644."
-fi
-###############################################################################
-# Move the config.py file to the proper location
-###############################################################################
-dialog --infobox "Moving the config.py file to the proper location..." 10 40
-sleep 1
-sudo mv ./config.py /etc/gustave/config.py
-if [ $? -eq 0 ]; then
-    log "Moved config.py into place."
-else
-    log "Failed to move config.py into place"
-fi
-###############################################################################
-# Set the owner and permissions for the config file
-###############################################################################
-dialog --infobox "Setting the owner and permissions for the config file..." 10 40
-sleep 1
-sudo chown gustave:gustave /etc/gustave/config.py
-if [ $? -eq 0 ]; then
-    log "Set owner /etc/gustave/config.py to gustave:gustave."
-else
-    log "Failed to set owner /etc/gustave/config.py to gustave:gustave."
-fi
-sleep 1
-sudo chmod 644 /etc/gustave/config.py
-if [ $? -eq 0 ]; then
-    log "modified /etc/gustave/config.py to 644."
-else
-    log "Failed to modify /etc/gustave/config.py to 644."
-fi
-sleep 1
-###############################################################################
-# Reload the systemd daemon to recognize the new service
-###############################################################################
-dialog --infobox "Reloading the systemd daemon to recognize the new service..." 10 40
-sudo systemctl daemon-reload
-if [ $? -eq 0 ]; then
-    log "Reloaded the systemd daemon."
-else
-    log "Failed to reload the systemd daemon."
-fi
-sleep 1
-###############################################################################
-# Enable the service so it starts on boot
-###############################################################################
-dialog --infobox "Enabling the service so it starts on boot..." 10 40
-sudo systemctl enable gustave
-if [ $? -eq 0 ]; then
-    log "Enabled the gustave service."
-else
-    log "Failed to enable the gustave service."
-fi
-sleep 1
-###############################################################################
-# Start the service
-###############################################################################
-dialog --infobox "Starting the service..." 10 40
-sudo systemctl start gustave
-if [ $? -eq 0 ]; then
-    log "Started the gustave service."
-else
-    log "Failed to start the gustave service."
-fi
-sleep 1
-###############################################################################
-#Finish
-###############################################################################
-dialog --msgbox "Installation complete!  Please examine the log to ensure there were no errors." 0 0
-clear
-exit 0
-###############################################################################
