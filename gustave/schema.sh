502bb00df179f43cdc93c9763dd12dde3afcd8f1
Chris <m.christopher.cohoon@me.com>
2023-07-19 12:13:50 -0500

    Update schema.sh

--- gustave/schema.sh
+++ gustave/schema.sh
@@ -9,8 +9,8 @@ sudo mysql -u root -e "CREATE USER '$mysql_user'@'localhost' IDENTIFIED BY '$mys
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON $mysql_db.* TO '$mysql_user'@'localhost';"
 sudo mysql -u root -e "FLUSH PRIVILEGES;"
 
-# Modified the secret_table creation query to include the id and is_active columns
-sudo mysql -u root -e "USE $mysql_db; CREATE TABLE secret_table (id INT AUTO_INCREMENT PRIMARY KEY, udid VARCHAR(255) NOT NULL UNIQUE, secret VARCHAR(255) NOT NULL, computer_id INT NOT NULL, expiration INT NOT NULL, is_active BOOLEAN DEFAULT TRUE);"
+# Updated the secret_table creation query to remove the UNIQUE constraint on the udid column
+sudo mysql -u root -e "USE $mysql_db; CREATE TABLE secret_table (id INT AUTO_INCREMENT PRIMARY KEY, udid VARCHAR(255) NOT NULL, secret VARCHAR(255) NOT NULL, computer_id INT NOT NULL, expiration INT NOT NULL, is_active BOOLEAN DEFAULT TRUE);"
 
 # The other tables remain unchanged
 sudo mysql -u root -e "USE $mysql_db; CREATE TABLE active_profiles (profile_id INT NOT NULL, computer_id INT NOT NULL, PRIMARY KEY (profile_id));"
