7aec50761aba0322cd0c6156f2983fea8ff204d2
Chris <m.christopher.cohoon@me.com>
2023-07-20 11:37:17 -0500

    celeryconfig.py added

--- gustave/celery_tasks.py
+++ gustave/celery_tasks.py
@@ -1,11 +1,47 @@
+##############################################
+# celery_tasks.py file
+##############################################
 import requests
 import logging
 from celery import Celery
+import importlib.util
+import os
 from services import generate_jamf_pro_token
+
+# Load config
+sys.path.append('/etc/gustave')
+# Add the path to the system path for direct imports
+sys.path.append('/etc/gustave')
+# Import the configuration
+spec = importlib.util.spec_from_file_location('config', '/etc/gustave/config.py')
+config_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(config_module)
+print(dir(config_module))
+# Extract the values
+JAMF_PRO_URL = config_module.Config.JAMF_PRO_URL
+JAMF_PRO_USERNAME = config_module.Config.JAMF_PRO_USERNAME
+JAMF_PRO_PASSWORD = config_module.Config.JAMF_PRO_PASSWORD
+CELERY_BROKER_URL = config_module.Config.CELERY_BROKER_URL
+CELERY_RESULT_BACKEND = config_module.Config.CELERY_RESULT_BACKEND
+
+def generate_jamf_pro_token():
+    url = JAMF_PRO_URL + '/uapi/auth/tokens'
+    auth = (JAMF_PRO_USERNAME, JAMF_PRO_PASSWORD)
+    headers = {"Accept": "application/json"}
+
+    response = requests.post(url, auth=auth, headers=headers)
+
+    if response.status_code == 200:
+        jamfToken = response.json().get('token')
+        return jamfToken
+    else:
+        logging.error(f"Failed to generate Jamf Pro API token: {response.content}")
+        raise Exception(f"Failed to generate Jamf Pro API token: {response.content}")
+
 
 # Create a Celery instance
 celery = Celery(__name__)
-
+celery.config_from_object('celeryconfig')
 def init_celery(flask_app):
     """
     Initialize Celery with the Flask app context.
