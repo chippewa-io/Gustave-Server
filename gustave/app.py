2dd8a58b2cd746a973bbf7c15b26ba905902f13e
Chris <m.christopher.cohoon@me.com>
2023-07-26 11:36:05 -0500

    Update app.py

--- gustave/app.py
+++ gustave/app.py
@@ -82,8 +82,8 @@ if __name__ == '__main__':
     from cleaner import run_cleaner
 
     # Start the activation check in a separate thread
-    #activation_thread = Thread(target=run_activation_check)
-    #activation_thread.start()
+    activation_thread = Thread(target=run_activation_check)
+    activation_thread.start()
 
     # Start the profile cleanup in a separate thread
     cleaner_thread = Thread(target=run_cleaner)
