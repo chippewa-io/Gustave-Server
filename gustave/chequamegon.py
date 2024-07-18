4f4e9cf26d52dac7950093e6b794a20f4d605344
chippewaChris <chris@chippewa.io>
2024-07-18 23:44:29 +0000

    Enhance chequamegon.py: implement logging, improve exception handling, remove unused import

--- gustave/chequamegon.py
+++ gustave/chequamegon.py
@@ -4,45 +4,56 @@ import os
 import sys
 import logging
 
-# import config
-#sys.path.append('/etc/gustave')
-print(sys.path)
+###############################################
+# Add the path to the custom config file
+sys.path.append('/etc/gustave')
+
+###############################################
+# Load config
 from gustave_config import ProductionConfig as Config
 
+###############################################
+# Setup Logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+###############################################
+# License Server Variables
 SERVER = "https://chequamegon.chippewa.io"
+CHECK_INTERVAL = 600  # 10 minutes
+SLEEP_ON_ERROR = 7 * 24 * 60 * 60  # 7 days
+
+###############################################
+# RUN
 def run_activation_check():
     while True:
-        time.sleep(5)
         license_key = Config.ACTIVATION_CODE
-        print("Starting activation check...")
-        print(f"Verifying license with Server: {SERVER}")
-        print(f"License key: {Config.ACTIVATION_CODE}")
+        logging.info("Starting activation check...")
+        logging.info(f"Verifying license with Server: {SERVER}")
+        logging.info(f"License key: {Config.ACTIVATION_CODE}")
         data = {"license_key": license_key}
+
         try:
             response = requests.post(f"{SERVER}/api/verify", json=data, timeout=60)  # Add timeout to handle potential hangs
             
-            # If server returns a non-200 and non-404 status code
             if response.status_code not in [200, 404]:
-                print(f"Unexpected status code {response.status_code}")
-                time.sleep(7 * 24 * 60 * 60)
+                logging.error(f"Unexpected status code {response.status_code}")
+                time.sleep(SLEEP_ON_ERROR)
                 continue
             
-            # If server returns a 200 OK status
             if response.status_code == 200:
                 result = response.json()
-                print (result)
+                logging.info(result)
                 
-                # Only halt the program when License is suspended
-                if result.get('message') == 'License suspended':  
+                if result.get('message') == 'License suspended':
                     os.kill(os.getpid(), signal.SIGINT)
                     
-            # If server returns a 404, handle it gracefully
             elif response.status_code == 404:
-                print("Received 404: License might not be found or there's an issue with the server")
+                logging.warning("Received 404: License might not be found or there's an issue with the server")
 
         except requests.RequestException as e:
-            print(f"Error contacting activation server: {e}")
+            logging.error(f"Error contacting activation server: {e}")
 
-        # Sleep for a time before checking again
-        time.sleep(600)
+        time.sleep(CHECK_INTERVAL)
+
+if __name__ == '__main__':
+    run_activation_check()
